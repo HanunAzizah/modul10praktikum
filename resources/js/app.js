c4f0336939e0e28cdadd2f7d16b9c4ccf186839f
HanunAzizah <117983620+HanunAzizah@users.noreply.github.com>
2023-07-31 12:20:54 +0700

    Add files via upload

---
+++ resources/js/app.js
@@ -0,0 +1,14 @@
+// import "./bootstrap";
+// import.meta.glob(["../images/**"]);
+
+
+// import './bootstrap';
+// import 'datatables.net-bs5';
+// import 'datatables.net-buttons-bs5';
+// import.meta.glob(["../images/**"]);
+
+import './bootstrap';
+import 'datatables.net-bs5';
+import 'datatables.net-buttons-bs5';
+import './employee';
+import.meta.glob(["../images/**"]);
