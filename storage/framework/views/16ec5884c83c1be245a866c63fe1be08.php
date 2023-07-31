da5826eddb7d4030e2618573dc62ea062135bf89
HanunAzizah <117983620+HanunAzizah@users.noreply.github.com>
2023-07-31 12:24:28 +0700

    Add files via upload

---
+++ storage/framework/views/16ec5884c83c1be245a866c63fe1be08.php
@@ -0,0 +1,9 @@
+<?php echo e($data['firstName']); ?>
+
+<?php echo e($data['lastName']); ?>
+
+<?php echo e($data['email']); ?>
+
+<?php echo e($data['age']); ?>
+
+<?php /**PATH C:\Laravel\Modul-4\resources\views/employee/pass.blade.php ENDPATH**/ ?>
